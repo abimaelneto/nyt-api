9b75982ab7d6259b74dccfa522830f57c0bdd161
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-06-14 14:17:29 -0300

    atualiza rota de detalhe de livros

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import ArticlesView from '../views/ArticlesView.vue'
-import BooksView from '../views/BooksView.vue'
-import BookDetailView from '../views/BookDetailView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import ArticlesView from "../views/ArticlesView.vue";
+import BooksView from "../views/BooksView.vue";
+import BookDetailView from "../views/BookDetailView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/articles',
-      name: 'articles',
+      path: "/articles",
+      name: "articles",
       component: ArticlesView,
     },
     {
-      path: '/books',
-      name: 'books',
-      component: BooksView
+      path: "/books",
+      name: "books",
+      component: BooksView,
     },
     {
-      path: '/detail/:isbn',
-      name: 'detail',
-      component: BookDetailView
+      path: "/books/:isbn",
+      name: "bookDetail",
+      component: BookDetailView,
     },
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
 // exports.key = "HRb1SQE0WMuY63JclAJXBlxHUAPxAfDl";
